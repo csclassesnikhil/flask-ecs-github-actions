be5440280be34744d5d796f6ae4bd1d39820dccc
csclassesnikhil <csclassesnikhil@gmail.com>
2025-12-01 02:00:32 -0800

    blow-commit

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return {"message": "Hello from Flask!"}
 
+@app.route("/hello")
+def home1():
+    return "hello master developer"    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
